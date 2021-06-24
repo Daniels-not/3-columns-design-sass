3cca23d0fce325223a743e87de58d806cb0a179c
Daniels-not <68081630+Daniels-not@users.noreply.github.com>
2021-06-24 23:28:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+//* compile scss to css
+
+function style(){
+    //? Where is my scss
+    return gulp.src('./Sass/**/*.scss')
+    //? pass the file through sass compiler
+    .pipe(sass().on('error', sass.logError))
+    //? where do i save the compiled css
+    .pipe(gulp.dest('./Css'))
+    //? Stream changes to all browers
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./Sass/**/*.scss', style);
+    gulp.watch('./*html').on('change', browserSync.reload);
+    //? strem any js file
+    //gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
