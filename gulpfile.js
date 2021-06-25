21d30f5fcac24b261dbdc3faf379b22e7924c7d7
Ramy Daniel Campusano Volquez <68081630+Daniels-not@users.noreply.github.com>
2021-06-25 15:50:42 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,32 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+//const gulp = require('gulp');
+//const sass = require('gulp-sass');
+//const browserSync = require('browser-sync').create();
 
 //* compile scss to css
 
-function style(){
+
+//function style(){
     //? Where is my scss
-    return gulp.src('./Sass/**/*.scss')
+    //return gulp.src('./Sass/**/*.scss')
     //? pass the file through sass compiler
-    .pipe(sass().on('error', sass.logError))
+    //.pipe(sass().on('error', sass.logError))
     //? where do i save the compiled css
-    .pipe(gulp.dest('./Css'))
+    //.pipe(gulp.dest('./Css'))
     //? Stream changes to all browers
-    .pipe(browserSync.stream());
+    //.pipe(browserSync.stream());
 }
 
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
+//function watch() {
+    //browserSync.init({
+       // server: {
+          //  baseDir: './'
         }
-    });
-    gulp.watch('./Sass/**/*.scss', style);
-    gulp.watch('./*html').on('change', browserSync.reload);
+   // });
+   // gulp.watch('./Sass/**/*.scss', style);
+  // gulp.watch('./*html').on('change', browserSync.reload);
     //? strem any js file
     //gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
-exports.style = style;
-exports.watch = watch;
+//exports.style = style;
+//exports.watch = watch;
